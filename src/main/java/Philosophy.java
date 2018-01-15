62d7b54e4945230f837891512a4b7f888ac81461
Tony Moretti <tony@moreready.com>
2018-01-15 09:30:20 -0600

    Improved finding the tags that invalidate a link

--- src/main/java/Philosophy.java
+++ src/main/java/Philosophy.java
@@ -37,13 +37,9 @@ public class Philosophy {
     public String findNextLink(Document currentPage){
         // Load the body of the article
         Element body = currentPage.body();
-        // The start of the actual article will always start with a paragraph tag
-        Elements paragraphs = body.select("p");
-        if(paragraphs.isEmpty()){
-            return null; // TODO: Decide if we should create exceptions or just have special messages
-        }
-        Elements wikiLinks = paragraphs.first().select("a[href^=/wiki/]");
-        if(wikiLinks.isEmpty()){
+        // Because the first link might not be in the fist paragraph, we need to check all the links
+        Elements wikiLinks = body.select("p > a[href^=/wiki/]");
+        if(wikiLinks.isEmpty()){ // Found an article with no links
             return null; // TODO: Decide if we should create exceptions or just have special messages
         }
         // Find the first link that is valid:
@@ -62,17 +58,33 @@ public class Philosophy {
                 continue;
             String surroundingHtml = link.parent().toString();
             String toStart = surroundingHtml.substring(0, surroundingHtml.indexOf(link.toString()));
-            // Strip out any complete parentheses pairs
-            while(toStart.contains("(") && toStart.contains(")")){
-                toStart = toStart.substring(toStart.indexOf(')') + 1);
-            }
-            while(toStart.contains("<i>") && toStart.contains("</i>")){
-                toStart = toStart.substring(toStart.indexOf("</i>") + 1);
-            }
-            if(!(toStart.contains("(") || toStart.contains("<i>")))
+            // Count open and closed tags to make sure they're balanced
+            if(specialBalanced(toStart))
                 return link;
         }
         return null;
+    }
+
+    private boolean specialBalanced(String htmlToTest){
+        int openParens = getCount(htmlToTest, "(");
+        int closedParens = getCount(htmlToTest, ")");
+        if(openParens != closedParens)
+            return false;
+
+        int openI = getCount(htmlToTest, "<i"); // Sometimes the tags contain more information
+        int closedI = getCount(htmlToTest, "</i>");
+        return openI == closedI;
+    }
+
+    private int getCount(String htmlToTest, String tagToFind){
+        int index = htmlToTest.indexOf(tagToFind);
+        int count = 0;
+        while(index != -1){
+            count++;
+            htmlToTest = htmlToTest.substring(index + 1);
+            index = htmlToTest.indexOf(tagToFind);
+        }
+        return count;
     }
 
     public String walkThePath(String url){
@@ -100,7 +112,7 @@ public class Philosophy {
 
     public static void main(String[] args) {
         Philosophy phil = new Philosophy();
-        String result = phil.walkThePath("https://en.wikipedia.org/wiki/Ankylosaurus");
+        String result = phil.walkThePath("https://en.wikipedia.org/wiki/Logic");
         System.out.println(result);
         System.out.println("Number of steps taken: " + phil.numberOfSteps);
         System.out.println("Path taken:");
