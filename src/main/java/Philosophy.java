a627eab12aba453ddcc7a73b64753a84fa845042
Tony Moretti <tony@moreready.com>
2018-01-14 18:44:41 -0600

    Make an HTTP GET request

--- src/main/java/Philosophy.java
+++ src/main/java/Philosophy.java
@@ -1,5 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.*;
+
 public class Philosophy {
-    public static int returnOne(){
-        return 1;
+    private HashSet<String> vistedPages;
+    private List<String> path;
+
+    public Philosophy(){
+        vistedPages = new HashSet<>();
+        path = new ArrayList<>();
+    }
+
+    public String loadPage(String input){
+        if(input == null || input.equals("") || vistedPages.contains(input))
+            return null; // TODO: Create an exception to show that there is a cycle? Or at least return an error message
+        StringBuilder result = new StringBuilder();
+        try {
+            // TODO: turn into actual api call and do validation
+            URL url = new URL(input);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            rd.close();
+            path.add(input);
+            vistedPages.add(input);
+            return result.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
--- src/test/java/PhilosophyTest.java
+++ src/test/java/PhilosophyTest.java
@@ -1,10 +1,43 @@
 import org.junit.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class PhilosophyTest {
+    Philosophy phil;
+
+    @Before
+    public void setUp(){
+        phil = new Philosophy();
+    }
+
+    @After
+    public void tearDown(){
+        phil = null;
+    }
+
     @Test
-    public void returnOneTest(){
-        assertEquals(Philosophy.returnOne(), 1);
+    public void loadPageNoInputTest(){
+        String result = phil.loadPage(null);
+        assertEquals(null, result);
+    }
+
+    @Test
+    public void loadPageEmptyInputTest(){
+        String result = phil.loadPage("");
+        assertEquals(null, result);
+    }
+
+    @Test
+    public void loadPageCycleTest(){
+        phil.loadPage("http://www.google.com/");
+        String result = phil.loadPage("http://www.google.com/");
+        assertEquals(null, result);
+    }
+
+    @Test
+    public void loadPageTest(){
+        String result = phil.loadPage("http://www.google.com/");
+        assertTrue(result.contains("Google"));
     }
 }
